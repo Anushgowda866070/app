74d7fa3342281f5462d628e01ad5f16df044bd93
prasannakumar.s <prasannakumarshetty2003@gmail.com>
2026-08-07 16:06:08 +0530

    Removed status code and Added loggers

--- src/main/java/com/payment/socket/GatewaySocketClient.java
+++ src/main/java/com/payment/socket/GatewaySocketClient.java
@@ -4,14 +4,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class GatewaySocketClient {
 
+    private static final Logger LOGGER=LoggerFactory.getLogger(GatewaySocketClient.class);
+
     public String sendRequest(String request) {
+
+        LOGGER.debug("Sending request to endpoint");
 
         try {
 
             Socket socket = new Socket("localhost", 4000);
+
+            LOGGER.info("Connected to endpoint successfully");
 
             PrintWriter writer =
                     new PrintWriter(socket.getOutputStream(), true);
@@ -24,12 +33,16 @@ public class GatewaySocketClient {
 
             String response = reader.readLine();
 
+            if(response== null ||response.isEmpty()){
+                LOGGER.warn("Received unexpected response from endpoint");
+            }
+
             socket.close();
 
             return response;
 
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error("Gateway connection Failed",e);
             return "Gateway Connection Failed";
         }
     }
