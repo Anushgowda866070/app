8b8bed9a2bdffc8bccacc5f9361748d88d04ea8a
anush <anush.ks@utthunga.com>
2026-08-11 15:48:51 +0530

    small changes

--- src/main/java/com/payment/GatewayApplication.java
+++ src/main/java/com/payment/GatewayApplication.java
@@ -1,20 +1,19 @@
-package com.payment.server;
+package com.payment;
 
 import com.payment.servlet.*;
 import com.sun.net.httpserver.HttpServer;
-
 import java.net.InetSocketAddress;
 
-public class App {
+public class GatewayApplication {
 
     public static void main(String[] args) throws Exception {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        server.createContext("/sale", new Servlet());
-        server.createContext("/refund", new Servlet());
-        server.createContext("/verify", new Servlet());
-        server.createContext("/void", new Servlet());
+        server.createContext("/gateway/sale", new Servlet());
+        server.createContext("/gateway/refund", new Servlet());
+        server.createContext("/gateway/verify", new Servlet());
+        server.createContext("/gateway/void", new Servlet());
 
         server.setExecutor(null);
 
